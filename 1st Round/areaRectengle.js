9e633361a5765b365e7c97b60d77d2aa614a7941
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-04-21 13:43:16 +0600

    write a funstion that calculates and prints the area of a rectangle give its lenght and width?

---
+++ 1st Round/areaRectengle.js
@@ -0,0 +1,24 @@
+// 3. write a funstion that calculates and prints the area of a rectangle give its lenght and width?
+
+function calculateArea( lenght, width ) {
+
+    if( lenght <= 0){
+        throw new RangeError("Lenght should be a positive number");
+        //  console.log("Lenght should be a positive number");
+    }
+
+    if( width <= 0){
+         throw new RangeError("Width should be a positive number");
+        //  console.log("Width should be a positive number");
+    }
+
+
+    const area = lenght * width;
+    console.log("The rectangle area is: ", area);
+}
+
+calculateArea(3, 3);
+
+calculateArea( 14, 867);
+ calculateArea( -4, 8);    // lenght is negative
+ calculateArea( 4, -5);    // Width is negative
