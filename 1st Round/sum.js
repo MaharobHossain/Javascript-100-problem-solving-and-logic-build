4fb81193eb6c853d0f6552c206407ba1a13ac018
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-04-21 13:10:01 +0600

    write a function that takes two number as input and print thier sum

---
+++ 1st Round/sum.js
@@ -0,0 +1,12 @@
+// 2. write a function that takes two number as input and print thier sum
+
+function sum(a, b) {
+    const sum = a + b;
+    console.log("Sum is: ", sum);
+}
+sum(2, 4);
+sum(-1, 2);
+sum(-1, -2);
+sum(1, 0);
+sum(1, 1.5);
+sum(2.5, 1.5);
